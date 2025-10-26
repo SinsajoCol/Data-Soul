879bd78ca09ebbfc7d15a5270ae00dcf0f7915cc
Emm3-z <munozunigaerika@gmail.com>
2025-10-26 15:49:57 -0500

    Interacción del NavBar
    - Corrección para la inyección de HTML directamente en la página por medio del DOM, par que todo lo maneje PaginaTemplate. Además, se adicionó un método para el manejo dinámico de las páginas active.
    - Se adicionaron los estilos para cuando las páginas estén active y se hizo la respectiva media query para la barra lateral y a su vez tenga su propia interacción.

--- src/assets/js/PaginaTemplate.js
+++ src/assets/js/PaginaTemplate.js
@@ -5,11 +5,14 @@ export class PaginaTemplate {
     const contenido = await this.mostrarContenido();
     const footer = this.mostrarFooter();
 
-    return `
+    document.body.innerHTML = `
       ${navbar}
       <main class="contenido">${contenido}</main>
       ${footer}
     `;
+
+    // Ahora que el HTML está en el DOM, se pueden agregar los eventos
+    this.agregarActive();
   }
 
   mostrarHeader() {
@@ -77,5 +80,27 @@ export class PaginaTemplate {
         </div>
       </footer>
   `;
+  }
+
+  agregarActive() {
+    const enlaces = document.querySelectorAll('.menu a');
+    const currentPage = document.body.getAttribute('data-page');
+
+    enlaces.forEach(e => {
+      e.classList.remove('active');
+      if (e.dataset.page === currentPage) {
+        e.classList.add('active');
+      }
+
+      e.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        enlaces.forEach(x => x.classList.remove('active'));
+        e.classList.add('active');
+
+        const page = e.dataset.page;
+        document.body.setAttribute('data-page', page);
+        this.cargarPagina(page); // método que renderiza la página
+      });
+    });
   }
 }
