f1b487bc4ed8fc1eb149488bf0bb503d026bd671
Juan Sebastian Obando <juanobando202@gmail.com>
2025-11-22 09:53:35 -0500

    Publicación del aplicativo

--- src/assets/js/PaginaPruebaGrupal.js
+++
@@ -1,9 +0,0 @@
-import { PaginaTemplate } from "./PaginaTemplate.js";
-
-export class PaginaPruebaGrupal extends PaginaTemplate {
-  async mostrarContenido() {
-    const response = await fetch("/src/pages/PruebaGrupal.html");
-    const html = await response.text();
-    return html;
-  }
-}
--- src/models/Rasgo.js
+++
@@ -1,6 +0,0 @@
-export class Rasgo {
-    constructor(nombre, valor = 0) {
-        this.nombre = nombre;
-        this.valor = valor;
-    }
-}
